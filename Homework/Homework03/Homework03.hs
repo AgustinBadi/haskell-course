5e39a8817afc40aa0e676ca0fdd982bee2a5df37
AgustinBadi <absalinas@uc.cl>
2023-01-06 15:57:43 -0300

    Homework nº3 ready

--- Homework/Homework03/Homework03.hs
+++ Homework/Homework03/Homework03.hs
@@ -4,7 +4,6 @@
 -- The function has to take the hourly consumption of an electrical device, the hours of daily use, and the maximum monthly consumption allowed.
 -- (Monthly usage = consumption (kW) * hours of daily use (h) * 30 days).
 
-
 -- Question 2
 -- Prelude:
 -- We use the function `show :: a -> String` to transform any type into a String.
@@ -12,10 +11,25 @@
 
 -- In the previous function, return the excess/savings of consumption as part of the message.
 
+electricalCheck :: Float -> Float -> Float -> [Char]
+electricalCheck consumption hoursPerDay maxConsumption = let currentConsume = consumption * hoursPerDay * 30
+   in if currentConsume > maxConsumption 
+      then "Maximum consumption exceeded by: " ++ show (currentConsume - maxConsumption) ++ " kW" 
+      else "consumption according to range limits, savings: " ++ show (maxConsumption - currentConsume) ++ " kW"
+
 
 -- Question 3
 -- Write a function that showcases the advantages of using let expressions to split a big expression into smaller ones.
 -- Then, share it with other students in Canvas.
+
+-- Without let expressions the code is more verbose.
+
+vocalCount name = "Your name "  ++ name ++ " has " ++ (show $ length [ x  | x <- name, elem x ['A','E','I','O','U','a','e','i','o','u' ]]) ++ " vocals such as " ++  [ x  | x <- name, elem x ['A','E','I','O','U','a','e','i','o','u' ]]
+
+-- With let expression is a lot more readable
+
+vocalCount' name = let vocals = [ x  | x <- name, elem x ['A','E','I','O','U','a','e','i','o','u' ]]
+ in "Your name "  ++ name ++ " has " ++ (show $ length vocals) ++ " vocals such as " ++ vocals
 
 
 -- Question 4
@@ -24,7 +38,22 @@
 -- possible. To implement this function using both guards and if-then-else statements.  
 
 
+quotient :: Float -> Float -> String
+quotient a b
+ | (a == 0) || (b == 0) = "Error: Division by zero is not possible"
+ | otherwise = if a / b > 1 then show $ b / a else show $ a / b
+
+
+
 -- Question 5
 -- Write a function that takes in two numbers and calculates the sum of squares for the product and quotient
 -- of those numbers. Write the function such that you use a where block inside a let expression and a
 -- let expression inside a where block. 
+
+
+-- ( a * b ) ^ 2 + (a / b ^ 2)
+
+squareOp a b = 
+ let squareProduct = sqrt multiplication where multiplication = a * b 
+ in squareProduct + squareQuotient
+ where squareQuotient = let quotient = a / b in sqrt quotient
