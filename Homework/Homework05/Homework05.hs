0c8bf66552688c8a76022d59f459dbdc0befb77b
AgustinBadi <absalinas@uc.cl>
2023-01-11 15:20:14 -0300

    Homework nº5 ready

--- Homework/Homework05/Homework05.hs
+++ Homework/Homework05/Homework05.hs
@@ -2,25 +2,40 @@
 -- flips the order of the parameters.
 -- For example this: `(/) 6 2` returns `3`. But this: `flip' (/) 6 2` returns `0.3333333333`
 
+--flipTwoParams :: (a -> )
+
+flip' :: (b -> a -> c) -> a -> b -> c
+flip' = \a b -> f a b
 
 -- Create the `uncurry'` function that converts a curried function to a function on pairs. So this: `(+) 1 2` that returns `3` can be written as
 -- `uncurry' (+) (1,2)` (with the two different arguments inside a pair).
 
+uncurry' :: (a -> b -> c) -> (a, b) -> c
+uncurry' f (a,b) = f a b 
 
 -- Create the `curry'` function that converts an uncurried function to a curried function. So this: `fst (1,2)` that returns `1` can be written as
 -- `curry' fst 1 2` (with the tuple converted into two different arguments).
 
+curry' :: ((a,b) -> c) -> a -> b -> c
+curry' f a b = f (a,b)
 
 -- Use higher-order functions, partial application, and point-free style to create a function that checks if a word has an uppercase letter.
 -- Start with using just higher-order functions and build from there. 
 
+checksUpper = any \x -> elem x ['A'..'Z']
 
 -- Create the `count` function that takes a team ("Red", "Blue", or "Green") and returns the amount of votes the team has inside `votes`.
 
 votes :: [String]
 votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
 
+
+count :: String -> Int
+count x = length . filter (== x) $ vote
+
 -- Create a one-line function that filters `cars` by brand and then checks if there are any left.
 
 cars :: [(String,Int)]
 cars = [("Toyota",0), ("Nissan",3), ("Ford",1)]
+
+checkCar car = snd . head .filter (\x -> fst x == car)  
